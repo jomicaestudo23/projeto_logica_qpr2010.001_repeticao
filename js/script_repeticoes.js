44f7949e473c91de4745da755a8a0716ea8402dd
jomicaestudo23 <joemyson.cardoso@aluno.senai.br>
2026-06-16 16:29:46 -0300

    criado endereço div for

--- js/script_repeticoes.js
+++ js/script_repeticoes.js
@@ -1,5 +1,5 @@
 //estrutura de repetição for
+const divfor = document.querySelector('#div-for')
 for ( i = 0; i < 10; i++ ){
-
-    console.log (i + 1  , 'amor')
+    console.log (i + 1  , 'amor')   
 }
