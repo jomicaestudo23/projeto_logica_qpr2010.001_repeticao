8d6ad8dea34f727e49c8ad2bbc782bc70f3673ff
jomicaestudo23 <joemyson.cardoso@aluno.senai.br>
2026-06-17 14:25:01 -0300

    feito o desafio e criação da questão 1

---
+++ js/script_repeticoes02.js
@@ -0,0 +1,43 @@
+//estrutura de repetição for
+const divfor = document.querySelector('#div-for')
+for ( i = 0; i < 10; i++ ){
+    console.log (i + 1  , 'amor <br>')
+    divfor.innerHTML += `${i} - Amor <br> `  
+}
+//contador/acumulador 
+const inputNum = document.querySelector('#num')
+const btnNum = document.querySelector('#btn-num')
+const divContAcum = document.querySelector('#div-cont-acum')
+
+let cont = 0
+let acum = 0.0
+
+btnNum.addEventListener('click', (evt)=>{
+    let numDigitado = String(inputNum.value)
+
+    cont++
+    acum += numDigitado
+
+    divContAcum.innerHTML = `TOTAL DE NÚMERO DIGITADO É: ${cont} <br> 
+    A SOMA DOS NÚMEROS É: ${acum}`
+
+})
+//  controlando for
+
+const inputFrase = document.querySelector('#frase')
+const inputNumRepeticao = document.querySelector('#num-repeticao')
+const btnFrase = document.querySelector ('#btn-frase')
+const divFrase = document.querySelector ('#div-frase')
+
+btnFrase.addEventListener('click', (evt)=>{
+    let numRepeti = Number(inputNumRepeticao.value)
+    let frase = inputFrase.value
+
+    divFrase.innerHTML = ''
+
+    for ( i = 0 ; i < numRepeti ; i++ ){
+
+        divFrase.innerHTML += ` ${frase} <br>`
+    }
+
+})
