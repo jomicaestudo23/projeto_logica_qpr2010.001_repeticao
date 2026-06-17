a63440bf840477ae006396ade7063e2ae9215fbc
jomicaestudo23 <joemyson.cardoso@aluno.senai.br>
2026-06-17 15:53:19 -0300

    treinamento for in e for of

--- js/script_repeticoes01.js
+++ js/script_repeticoes01.js
@@ -1,43 +1,46 @@
-//estrutura de repetição for
-const divfor = document.querySelector('#div-for')
-for ( i = 0; i < 10; i++ ){
-    console.log (i + 1  , 'amor <br>')
-    divfor.innerHTML += `${i} - Amor <br> `  
-}
-//contador/acumulador 
-const inputNum = document.querySelector('#num')
-const btnNum = document.querySelector('#btn-num')
-const divContAcum = document.querySelector('#div-cont-acum')
-
-let cont = 0
-let acum = 0.0
-
-btnNum.addEventListener('click', (evt)=>{
-    let numDigitado = String(inputNum.value)
-
-    cont++
-    acum += numDigitado
-
-    divContAcum.innerHTML = `TOTAL DE NÚMERO DIGITADO É: ${cont} <br> 
-    A SOMA DOS NÚMEROS É: ${acum}`
-
-})
 //  controlando for
 
-const inputFrase = document.querySelector('#frase')
+const inputFrase = document.querySelector('#numero')
 const inputNumRepeticao = document.querySelector('#num-repeticao')
 const btnFrase = document.querySelector ('#btn-frase')
 const divFrase = document.querySelector ('#div-frase')
 
 btnFrase.addEventListener('click', (evt)=>{
-    let numRepeti = Number(inputNumRepeticao.value)
-    let frase = inputFrase.value
+    
+    let contador = 0;
+
+    for ( contador = 0; contador < 1000; contador ++ ){
+    
+    if ( i % 7 === 0)
+
+    
 
     divFrase.innerHTML = ''
+    
 
-    for ( i = 0 ; i < numRepeti ; i++ ){
-
-        divFrase.innerHTML += ` ${frase} <br>`
+        divFrase.innerHTML += ` ${contador} <br>`
     }
 
 })
+
+//coleção de dados - array
+const  presentes = [ ' bicicleta', 'celular', 'camisa do mengão', 'doleira',
+'tênis adidas']
+const divArray = document.querySelector('#div-array')
+
+for(i = 0; i < 5; i++ )
+    divArray.innerHTML = `${presentes}` 
+// for in 
+const divForIn = document.querySelector('#div-forin')
+    
+for(let posicao in presentes){
+    divForIn.innerHTML += `${presentes[posicao]} <br>`
+
+}
+//for of
+const divForOf = document.querySelector('#div-forof')
+
+for(let elemento of presentes){
+divForOf.innerHTML += `${elemento}<br>`
+
+}
